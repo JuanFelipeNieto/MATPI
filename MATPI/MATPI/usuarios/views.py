8eed5a8cd51023fb54b0a1ed437012a69e777649
JuanNieto3001 <juanfenietoc@gmail.com>
2026-06-21 16:17:33 -0500

    arreglo reporte

--- MATPI/MATPI/usuarios/views.py
+++ MATPI/MATPI/usuarios/views.py
@@ -698,6 +698,29 @@ def reporte_modulo_pdf(request, modulo, periodo):
         qs = facturas
         template_path = 'reportes/pdf_facturas.html'
         titulo = "Reporte de Facturas"
+    elif modulo in ['pedidos', 'ventas']:
+        from django.db.models.functions import Coalesce
+
+        ordenar = request.GET.get('ordenar', '')
+
+        # Annotate each order with total quantity of products (sum of detalles__cantidad)
+        pedidos = pedidos_completados.annotate(
+            total_productos=Coalesce(
+                models.Sum('detalles__cantidad'),
+                0
+            )
+        )
+
+        if ordenar == 'cantidad_desc':
+            pedidos = pedidos.order_by('-total_productos')
+        elif ordenar == 'cantidad_asc':
+            pedidos = pedidos.order_by('total_productos')
+        else:
+            pedidos = pedidos.order_by('-fecha')
+
+        qs = pedidos
+        template_path = 'reportes/pdf_pedidos.html'
+        titulo = "Reporte de Pedidos"
     else:
         qs, template_path = config_reporte.get(modulo, (None, ""))
         titulo = f"Reporte de {modulo.capitalize()}"
